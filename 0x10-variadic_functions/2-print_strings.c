7fc5301ea13ad879d0a3cbd546562522acfce04b
FayidAhmed <fayidahm333@gmail.com>
2022-04-14 04:47:56 -0700

    space

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,11 +14,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 	char *c;
+
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
 		c = va_arg(list, char*);
+
 		if (c != NULL)
 			printf("%s", c);
 		else
